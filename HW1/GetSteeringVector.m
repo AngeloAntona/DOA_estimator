b0153cefa9ad6b4e1fcbcbc29dfc27e593590a91
alessandromanattini <82643655+alessandromanattini@users.noreply.github.com>
2024-05-07 17:33:53 +0200

    Update GetSteeringVector.m

--- HW1/GetSteeringVector.m
+++ HW1/GetSteeringVector.m
@@ -6,6 +6,8 @@ function a = GetSteeringVector(theta, d, c, numMics, freq)
     %   c - speed of sound in m/s
     %   numMics - number of microphones in the array
     %   freq - frequency in Hz for which to calculate the steering vector
+    % Output:
+    % a = steering vector
     
     k = 2 * pi * freq / c;  % Wavenumber for the given frequency
     a = exp(-1i * k * d * sin(deg2rad(theta)) * (0:(numMics-1))).';  % Steering vector, column vector
